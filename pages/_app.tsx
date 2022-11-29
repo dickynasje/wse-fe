1a4972551e7465f92f7dbad01fc79d7d7135c926
dickynasje <dickyarianugraha@gmail.com>
2022-11-29 14:40:28 +0700

    Progress FrontEnd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme, baseTheme } from '@chakra-ui/react'
+
+const colors = {
+  initialColorMode: "dark",
+}
+const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
